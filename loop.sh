0816f3017473b2704f00e2ebf6865c8a81984d2d
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-12-16 09:19:30 -0800

    fix to use compose docker plugin istead of standalone docker-compose binary

--- loop.sh
+++ loop.sh
@@ -2,11 +2,11 @@
 
 # for error "network bridge not found" do sudo ~/code/bash_configs/nas/change-iptables-bridge-docker-settings.sh 
 # TODO check if build was successfull
-docker-compose build
+docker compose build
 
 while true; do
-    docker-compose up
-    docker-compose down
+    docker compose up
+    docker compose down
     date
     sleep 3600
 done
