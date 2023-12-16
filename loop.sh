80b532c59ebf18e153a0e57bbc1e9d5622137fff
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-12-16 18:15:57 +0100

    add docker

---
+++ loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# for error "network bridge not found" do sudo ~/code/bash_configs/nas/change-iptables-bridge-docker-settings.sh 
+# TODO check if build was successfull
+docker-compose build
+
+while true; do
+    docker-compose up
+    docker-compose down
+    date
+    sleep 3600
+done
+
