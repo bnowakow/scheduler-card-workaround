3ba3e557d8bc9089b5b86d96db98b45a13f070d1
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2023-12-26 13:20:35 +0100

    add TODOs

--- src/main/kotlin/pl/bnowakowski/home_assistant_workaround/Main.kt
+++ src/main/kotlin/pl/bnowakowski/home_assistant_workaround/Main.kt
@@ -7,9 +7,16 @@ fun main() {
 
     val homeAssistant = HomeAssistant()
 
+    // TODO read args and execute 1 or 2 or 1 and 2
+
+    // 1
     homeAssistant.login()
     homeAssistant.iterateThroughSchedulesAndToggleThem()
+
+    //2
     homeAssistant.iterateThroughSwitchesInAlwaysOnGroupAndToggleThem()
+
+    // run always
     homeAssistant.finish()
 
     exitProcess(0)
